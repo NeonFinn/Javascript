c4bcf133c4e92ca014d5b4a1c82128d51a99f527
NeonFinn <n3onfinn@gmail.com>
2025-07-23 16:06:21 -0400

    -

--- Exercise 10/moz-todo-react/src/App.jsx
+++ Exercise 10/moz-todo-react/src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import cLogo from "./assets/logo.png";
 
 function App(props) {
   return (
     <>
       <header>
-        <h1>Hello, {props.subject}!</h1>
+        <img src={cLogo} className="logo" alt="Class Logo" />
+        <h1>Hello {props.subject}, have you met {props.name}?</h1>
+        <h2> Login to Get Started </h2>
+                <form>
+               <div class="wrap-input100" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="EMAIL" />
+						
+						
+					</div>
+				<div class="wrap-input100" data-validate="Password is required">
+						<input class="input100" type="password" name="pass" placeholder="PASSWORD" />
+						
+						
+					</div>
+        
+                <p class="button">
+                    <input type="submit" name="button" id="button" value="Login" />
+                </p>
+	</form>  
         <button type="button" className="primary">
           Click me!
         </button>
